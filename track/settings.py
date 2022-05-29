98265791e803094f923997faba0b6b77812ea3bf
negeek <negeek5ive@gmail.com>
2022-05-29 15:39:01 +0100

    changed database

--- track/settings.py
+++ track/settings.py
@@ -131,9 +131,14 @@ WSGI_APPLICATION = 'track.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'trackDb',
+        'USER': 'postgres',
+        'PASSWORD': 'dlion5ive',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
