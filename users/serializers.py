c0616c4112da1427a3710cb1ee2c30b1881c2cff
negeek <negeek5ive@gmail.com>
2022-05-29 22:55:52 +0100

    solved table errors

--- users/serializers.py
+++ users/serializers.py
@@ -16,12 +16,10 @@ class CustomRegisterSerializer(RegisterSerializer):
         super().__init__(*args, **kwargs)
         del self.fields['password2']
 
-    '''    
-
     def validate(self, data):
         if data['password1'] == ' ':
             raise serializers.ValidationError(_("input password!."))
-        return data'''
+        return data
 
     def save(self, request):
         user = super().save(request)
@@ -37,13 +35,13 @@ class CustomLoginSerializer(LoginSerializer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         del self.fields['username']
-
+    '''
     def _validate_email(self, email, password):
         if email and password:
             user = self.authenticate(email=email, password=password)
         else:
             msg = _('Must include "email" and "password".')
-            raise exceptions.ValidationError(msg)
+            raise exceptions.ValidationError(msg)'''
 
     def _validate_username(self, username, password):
         pass
