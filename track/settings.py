97c3652cf17157341ffc3a74b5f391d896749349
negeek <negeek5ive@gmail.com>
2022-05-30 01:02:55 +0100

    env

--- track/settings.py
+++ track/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -139,7 +139,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'trackDb',
         'USER': 'postgres',
-        'PASSWORD': env('DB_PASSWORD'),
+        'PASSWORD': env("DB_PASSWORD"),
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
